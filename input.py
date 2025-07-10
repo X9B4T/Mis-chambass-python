15e1a991a81dbac19fcbed7c459cfe4c8c381849
X9B4T <dmitricorleone@gmail.com>
2025-07-09 20:07:34 -0600

    Add files via upload

---
+++ input.py
@@ -0,0 +1,41 @@
+#Pregunta hacia como se llama
+print("Hola, como te llamas?")
+nombre=input()
+nombre1="Rodrigo"
+print(nombre)
+print(f"Que gusto conocerte {nombre}, mi nombre es {nombre1}")
+#Pregunta hacia su profesion
+print(f"¿A que te dedicas, {nombre}?")
+profesion=input()
+print(f"Super,{nombre}, yo me dedico a vender tenis")
+#Pregunta hacia su edad
+print(f"¿Y que edad tienes, {nombre}?")
+edad=input()
+edad1=25
+print(f"Orale, casi somos de la misma edad {nombre}, yo tengo {edad1}")
+#Pasara a presentar algunos integrantes del grupo
+print(f"Ahora iremos a presentarte a algunas personas que son de nuestro grupo, te van a caer bien, {nombre}")
+#Procede a presentarlos
+nombre2="Vanessa"
+print(f"Mira {nombre}, ella es {nombre2}")
+print("Ahora que ya se conocen pueden irse conociendo")
+#"Nombre1" deja a "nombre" con "nombre2" para que conozca a las demas personas de su grupo
+print(f"Un gusto conocerte {nombre}, vamos a presentarte a mis amigos")
+#"nombre2" lleva "nombre" con sus amigos para que los conozca
+print(f"Hola chicos, el es {nombre} se los vengo a presentar")
+#"nombre" conoce al primer chico "nombre3"
+nombre3="Gerardo"
+profesion0="administrador de empresas"
+edad2=22
+print(f"Hola {nombre}, me llamo {nombre3} es un gusto conocerte, pareces tener mi edad, ¿Que edad tienes, {nombre}?")
+edad=input()
+print(f"Excelente, yo tengo {edad2} ¿y a que de gustaria dedicarte {nombre}?")
+profesion=input()
+print(f"Eso suena dificil, pero se que lo lograras {nombre}, yo ando estudiando para ser {profesion0}")
+#Ahora "nombre" sigue a conocer a la 2da persona
+nombre4="Julia"
+profesion1="diseño grafico"
+print(f"Hola, {nombre}, me llamo {nombre4}")
+print(f"¿Y por ejemplo a que te gustaria dedicarte a ti {nombre4}?")
+print(f"A mi me gustaria dedicarme a{profesion1}")
+#Ya, asi se acabo todo, fin.
---
+++ variables.py
@@ -0,0 +1,34 @@
+print("Edades y presentaciones de jovenes del salon")
+
+#Nombre, edad y estatura de la maestra
+nombre="Juana Ines Cortes" 
+edad="37 años"
+estatura="1.63"
+print(f"Hola me presento, me llamo {nombre}, tengo {edad} y mido {estatura}. \nAhora les toca presentarse a ustedes, tambien odio a los Judios") 
+#Nombre, edad y estatura del 1er alumno
+nombre="Luis Ramirez Melendez" 
+edad = "21 años"
+estatura = "1.85 cm"
+print(f"Hola me llamo {nombre}, tenngo {edad} y mido {estatura} e igual estoy de acuerdo con la maestra.")
+
+print("¿Y tu que edad tienen Maria y cuando mides?")
+#Nombre, edad y estatura de la 2da alumna
+nombre="Maria Espinoza Paz"
+edad="25 años"
+estatura="1.65"
+print(f"Hola me llamo {nombre}, yo tengo {edad} y mido {estatura}, soy lesbiana, mi Papa nos abandono, soy pro palestina, tengo 175 generos fluidos y huelo a caca")
+
+#Nombre, edad y estatura del 3er alumno
+nombre="Raymundo Lopez Mejia "
+edad="23 años"
+estatura="1.75"
+print(f"Hola me llamo {nombre}, tengo {edad} y mido {estatura} y con la particicpacion de mi compañera anterior, concuerdo de que su papa la haya abandonado y valga muy verga e igual en efecto, huele a mierda")
+#Nombre, edad y estatura de la 4ta alumna.
+nombre="Ana Karen Abrego Flores"
+edad="18 años"
+estatura="1.66"
+print(f"Hola me llamo {nombre}, mido {estatura} y tengo {edad} y a mi me cagan especialmente los perros como Raymundo")
+
+
+
+
